0a611fc98717d06e1aa1df7b6ff7899fab30860e
antrmarq <antmarq78@gmail.com>
2024-02-06 10:26:40 -0700

    intital commit

--- convex_hull.py
+++ convex_hull.py
@@ -8,9 +8,14 @@ elif PYQT_VER == 'PYQT6':
 else:
 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
-
-
 import time
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
 
 # Some global color constants that might be useful
 RED = (255,0,0)
@@ -66,34 +71,159 @@ class ConvexHullSolver(QObject):
 
 		t1 = time.time()
 		# TODO: SORT THE POINTS BY INCREASING X-VALUE
-		points.sort(key=lambda p: p.x())
+		points.sort(key=lambda point: point.x())
 		t2 = time.time()
 
 		t3 = time.time()
 		# this is a dummy polygon of the first 3 unsorted points
 		# polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
-		polygon = self.divideAndConquer(points)
-		polygon = [QLineF(points[i],points[(i+1)%len(points)]) for i in range(len(points))]
+
+		node = divideConquer(points)
+		currNode = node
+		nodes = []
+		while True:
+			nextNode = currNode.next
+			nodes.append(QLineF(currNode.data, nextNode.data))
+			currNode = nextNode
+			if currNode == node:
+				break
+
 		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
 		t4 = time.time()
 
 		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
 		# object can be created with two QPointF objects corresponding to the endpoints
-		self.showHull(polygon,RED)
+		self.showHull(nodes,RED)
 		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
 
+def divideConquer(points):
+	if (len(points) == 2):
+		node1 = Node(points[0])
+		node2 = Node(points[1])
+		node1.next = node2
+		node1.prev = node2
+		node2.next = node1
+		node2.prev = node1
+		return node1
+	if (len(points) == 3):
+		node1 = Node(points[0])
+		node2 = Node(points[1])
+		node3 = Node(points[2])
+		slope2 = slopeFinder(node1, node2)
+		slope3 = slopeFinder(node1, node3)
+		if (slope2 > slope3):
+			node1.next = node2
+			node2.next = node3
+			node3.next = node1
+			node1.prev = node3
+			node3.prev = node2
+			node2.prev = node1
+		else:
+			node1.next = node3
+			node3.next = node2
+			node2.next = node1
+			node1.prev = node2
+			node2.prev = node3
+			node3.prev = node1
+		return node1
+	L,R = splitLR(points)
+	left = divideConquer(L)
+	right = divideConquer(R)
+	return findTangents(left,right)
 
-	def divideAndConquer(self, points):
-		if len(points) <= 3:
-			return [QLineF(points[i],points[(i+1)%len(points)]) for i in range(len(points))]
-		mid = len(points) // 2
-		left = self.divideAndConquer(points[:mid])
-		right = self.divideAndConquer(points[mid:])
-		return self.merge(left, right)
-	
-	def merge(self, left, right):
-		left = self.removeDuplicatePoints(left)
-		right = self.removeDuplicatePoints(right)
-		upper_tangent = self.findUpperTangent(left, right)
-		lower_tangent = self.findLowerTangent(left, right)
-		return upper_tangent + lower_tangent
+def splitLR(points):
+	mid = len(points) // 2
+	L = points[:mid]
+	R = points[mid:]
+	return L,R
+
+def slopeFinder(n1, n2):
+	return ((n2.data.y() - n1.data.y())/(n2.data.x() - n1.data.x()))
+
+def findMax(L):
+	head = L
+	maxX = L.data.x()
+	maxNode = L
+	current_node = L.next
+
+	while current_node is not head:
+		if current_node.data.x() > maxX:
+			maxX = current_node.data.x()
+			maxNode = current_node
+		current_node = current_node.next
+
+	return maxNode
+
+def findMin(R):
+	head = R
+	minX = R.data.x()
+	minNode = R
+	current_node = R.next
+
+	while current_node is not head:
+		if current_node.data.x() < minX:
+			minX = current_node.data.x()
+			minNode = current_node
+		current_node = current_node.next
+
+	return minNode
+
+def findTangents(L,R):
+
+	leftMax = findMax(L)
+	rightMin = findMin(R)
+	l = leftMax
+	r = rightMin
+	lc = leftMax
+	rc = rightMin
+
+	isLeft = True
+	leftDone = False
+	rightDone = False
+	while not (leftDone and rightDone):
+		if (isLeft):
+			preslopeL = slopeFinder(l,r)
+			postslopeL = slopeFinder(l.prev,r)
+			if (preslopeL > postslopeL):
+				l = l.prev
+				rightDone = False
+			else:
+				leftDone = True
+		else:
+			preslopeR = slopeFinder(l,r)
+			postslopeR = slopeFinder(l,r.next)
+			if (preslopeR < postslopeR):
+				r = r.next
+				leftDone = False
+			else:
+				rightDone = True
+		isLeft = not isLeft
+
+	isLeft = True
+	leftDone = False
+	rightDone = False
+	while not (leftDone and rightDone):
+		if (isLeft):
+			preslopeL = slopeFinder(lc,rc)
+			postslopeL = slopeFinder(lc.next,rc)
+			if (preslopeL < postslopeL):
+				lc = lc.next
+				rightDone = False
+			else:
+				leftDone = True
+		else:
+			preslopeR = slopeFinder(lc,rc)
+			postslopeR = slopeFinder(lc,rc.prev)
+			if (preslopeR > postslopeR):
+				rc = rc.prev
+				leftDone = False
+			else:
+				rightDone = True
+		isLeft = not isLeft
+
+	l.next = r
+	r.prev = l
+	rc.next = lc
+	lc.prev = rc
+
+	return rc
