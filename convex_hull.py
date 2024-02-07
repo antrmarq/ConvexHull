dfab7ecea7ae989222e8a67c3860c343655d72be
antrmarq <antmarq78@gmail.com>
2024-02-06 22:08:44 -0700

    changes

--- convex_hull.py
+++ convex_hull.py
@@ -8,14 +8,9 @@ elif PYQT_VER == 'PYQT6':
 else:
 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
+
+
 import time
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
-
 
 # Some global color constants that might be useful
 RED = (255,0,0)
@@ -70,160 +65,134 @@ class ConvexHullSolver(QObject):
 		assert( type(points) == list and type(points[0]) == QPointF )
 
 		t1 = time.time()
-		# TODO: SORT THE POINTS BY INCREASING X-VALUE
-		points.sort(key=lambda point: point.x())
+		sortedPoints = sorted(points, key=lambda k: k.x())
 		t2 = time.time()
 
 		t3 = time.time()
-		# this is a dummy polygon of the first 3 unsorted points
-		# polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
 
-		node = divideConquer(points)
-		currNode = node
-		nodes = []
-		while True:
-			nextNode = currNode.next
-			nodes.append(QLineF(currNode.data, nextNode.data))
-			currNode = nextNode
-			if currNode == node:
-				break
-
-		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
+		node = divide_and_conquer(sortedPoints)
+		polygon = to_qlinef_list(node)
 		t4 = time.time()
 
-		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-		# object can be created with two QPointF objects corresponding to the endpoints
-		self.showHull(nodes,RED)
+		self.showHull(polygon,RED)
 		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
 
-def divideConquer(points):
-	if (len(points) == 2):
-		node1 = Node(points[0])
-		node2 = Node(points[1])
-		node1.next = node2
-		node1.prev = node2
-		node2.next = node1
-		node2.prev = node1
-		return node1
-	if (len(points) == 3):
-		node1 = Node(points[0])
-		node2 = Node(points[1])
-		node3 = Node(points[2])
-		slope2 = slopeFinder(node1, node2)
-		slope3 = slopeFinder(node1, node3)
-		if (slope2 > slope3):
-			node1.next = node2
-			node2.next = node3
-			node3.next = node1
-			node1.prev = node3
-			node3.prev = node2
-			node2.prev = node1
-		else:
-			node1.next = node3
-			node3.next = node2
-			node2.next = node1
-			node1.prev = node2
-			node2.prev = node3
-			node3.prev = node1
-		return node1
-	L,R = splitLR(points)
-	left = divideConquer(L)
-	right = divideConquer(R)
-	return findTangents(left,right)
+def divide_and_conquer(points):
+	# Divide and conquer algorithm to find the convex hull of a set of points
+    num_points = len(points)
+    if num_points <= 1:
+        hull_root_node = double_linked_list(points)
+        return hull_root_node
 
-def splitLR(points):
-	mid = len(points) // 2
-	L = points[:mid]
-	R = points[mid:]
-	return L,R
+    left_hull_root = divide_and_conquer(points[:num_points // 2])
+    right_hull_root = divide_and_conquer(points[num_points // 2:])
 
-def slopeFinder(n1, n2):
-	return ((n2.data.y() - n1.data.y())/(n2.data.x() - n1.data.x()))
+    upper_left_tangent_node, upper_right_tangent_node, lower_left_tangent_node, lower_right_tangent_node = find_tangents(left_hull_root, right_hull_root)
 
-def findMax(L):
-	head = L
-	maxX = L.data.x()
-	maxNode = L
-	current_node = L.next
+    # Connect upper tangent
+    upper_left_tangent_node.next = upper_right_tangent_node
+    upper_right_tangent_node.prev = upper_left_tangent_node
 
-	while current_node is not head:
-		if current_node.data.x() > maxX:
-			maxX = current_node.data.x()
-			maxNode = current_node
-		current_node = current_node.next
+    # Connect lower tangent
+    lower_right_tangent_node.next = lower_left_tangent_node
+    lower_left_tangent_node.prev = lower_right_tangent_node
 
-	return maxNode
+    return upper_left_tangent_node
 
-def findMin(R):
-	head = R
-	minX = R.data.x()
-	minNode = R
-	current_node = R.next
+def find_tangents(left, right):
+	# Find the upper and lower tangents of two convex hulls
+	left_hull_right_most = find_right_node(left)
+	right_hull_left_most = find_left_node(right)
 
-	while current_node is not head:
-		if current_node.data.x() < minX:
-			minX = current_node.data.x()
-			minNode = current_node
-		current_node = current_node.next
+	left_tangent_upper_point = left_hull_right_most
+	right_tangent_upper_point = right_hull_left_most
+	done = True
+	while done:
+		done = False
+		previous_slope = slope(left_tangent_upper_point.point, right_tangent_upper_point.point)
 
-	return minNode
+		while slope(left_tangent_upper_point.prev.point, right_tangent_upper_point.point) < previous_slope:
+			left_tangent_upper_point = left_tangent_upper_point.prev
+			previous_slope = slope(left_tangent_upper_point.point, right_tangent_upper_point.point)
+			done = True
 
-def findTangents(L,R):
+		while slope(left_tangent_upper_point.point, right_tangent_upper_point.next.point) > previous_slope:
+			right_tangent_upper_point = right_tangent_upper_point.next
+			previous_slope = slope(left_tangent_upper_point.point, right_tangent_upper_point.point)
+			done = True
 
-	leftMax = findMax(L)
-	rightMin = findMin(R)
-	l = leftMax
-	r = rightMin
-	lc = leftMax
-	rc = rightMin
+	left_tangent_lower_point = left_hull_right_most
+	right_tangent_lower_point = right_hull_left_most
+	done = True
+	while done:
+		done = False
+		previous_slope = slope(left_tangent_lower_point.point, right_tangent_lower_point.point)
 
-	isLeft = True
-	leftDone = False
-	rightDone = False
-	while not (leftDone and rightDone):
-		if (isLeft):
-			preslopeL = slopeFinder(l,r)
-			postslopeL = slopeFinder(l.prev,r)
-			if (preslopeL > postslopeL):
-				l = l.prev
-				rightDone = False
-			else:
-				leftDone = True
-		else:
-			preslopeR = slopeFinder(l,r)
-			postslopeR = slopeFinder(l,r.next)
-			if (preslopeR < postslopeR):
-				r = r.next
-				leftDone = False
-			else:
-				rightDone = True
-		isLeft = not isLeft
+		while slope(left_tangent_lower_point.next.point, right_tangent_lower_point.point) > previous_slope:
+			left_tangent_lower_point = left_tangent_lower_point.next
+			previous_slope = slope(left_tangent_lower_point.point, right_tangent_lower_point.point)
+			done = True
 
-	isLeft = True
-	leftDone = False
-	rightDone = False
-	while not (leftDone and rightDone):
-		if (isLeft):
-			preslopeL = slopeFinder(lc,rc)
-			postslopeL = slopeFinder(lc.next,rc)
-			if (preslopeL < postslopeL):
-				lc = lc.next
-				rightDone = False
-			else:
-				leftDone = True
-		else:
-			preslopeR = slopeFinder(lc,rc)
-			postslopeR = slopeFinder(lc,rc.prev)
-			if (preslopeR > postslopeR):
-				rc = rc.prev
-				leftDone = False
-			else:
-				rightDone = True
-		isLeft = not isLeft
+		while slope(left_tangent_lower_point.point, right_tangent_lower_point.prev.point) < previous_slope:
+			right_tangent_lower_point = right_tangent_lower_point.prev
+			previous_slope = slope(left_tangent_lower_point.point, right_tangent_lower_point.point)
+			done = True
 
-	l.next = r
-	r.prev = l
-	rc.next = lc
-	lc.prev = rc
+	return left_tangent_upper_point, right_tangent_upper_point, left_tangent_lower_point, right_tangent_lower_point
 
-	return rc
+
+def slope(p1, p2):
+	# Calculate the slope of the line between two points
+	return (p2.y() - p1.y()) / (p2.x() - p1.x())
+
+
+class Node:
+	# Helper class for finding the nodes of a circular doubly linked list
+	def __init__(self, point):
+		self.point = point
+		self.prev = None
+		self.next = None
+
+def double_linked_list(points):
+	# Create a circular doubly linked list from a list of points
+	head = Node(points[0])
+	tail = head
+	for point in points[1:]:
+		node = Node(point)
+		node.prev = tail
+		tail.next = node
+		tail = node
+	head.prev = tail
+	tail.next = head
+	return head
+
+def to_qlinef_list(head):
+	# Convert a circular doubly linked list to a list of QLineF
+	qlinef_list = []
+	node = head
+	while True:
+		qlinef_list.append(QLineF(node.point, node.next.point))
+		node = node.next
+		if node == head:
+			break
+	return qlinef_list
+
+def find_right_node(head):
+	# Find the rightmost node in a circular doubly linked list
+	node = head
+	rightmost_node = node
+	while node.next != head:
+		node = node.next
+		if node.point.x() > rightmost_node.point.x():
+			rightmost_node = node
+	return rightmost_node
+
+def find_left_node(head):
+	# Find the leftmost node in a circular doubly linked list
+	node = head
+	leftmost_node = node
+	while node.next != head:
+		node = node.next
+		if node.point.x() < leftmost_node.point.x():
+			leftmost_node = node
+	return leftmost_node
